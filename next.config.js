905161ddc1c77b364de19bc5dd6bea2fc0049912
umitahmedd <135972028+umitahmedd@users.noreply.github.com>
2023-12-28 01:20:50 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["example.com"], // kullanılan resimlerin domain'ini ekleyin
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
