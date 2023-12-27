1fb4fc73cd84f95ec3aa4a171a38f4763e84c864
umitahmedd <135972028+umitahmedd@users.noreply.github.com>
2023-12-27 22:19:55 +0300

    Create HomeView.jsx

---
+++ src/app/Views/HomeView.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CustomButton from "@/app/Components/CustomButton";
+
+const HomeView = () => {
+  return (
+    <div>
+      <CustomButton buttonType={"addButton"} roundSize={2}>
+        Merhaba
+      </CustomButton>
+    </div>
+  );
+};
+
+export default HomeView;
