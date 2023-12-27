2a59f9beeb161325197448e0628c3f0a84cf62c9
umitahmedd <135972028+umitahmedd@users.noreply.github.com>
2023-12-28 01:21:47 +0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
 "use client";
 import HomeView from "@/app/Views/HomeView";
+import Hero from "@/app/Components/Hero";
 
 export default function Home() {
   return (
-    <>
-      <HomeView />
-    </>
+    <div className="bg-theme-gray-600 min-h-[100vh]">
+      <Hero />
+      <div className="w-full max-w-7xl mx-auto px-3">
+        <HomeView />
+      </div>
+    </div>
   );
 }
