9d36e615b4ee161fb0b5d51adaafeaa2f8278b48
umitahmedd <135972028+umitahmedd@users.noreply.github.com>
2023-12-30 16:59:15 +0300

    Update HomeView.jsx

--- src/app/Views/HomeView.jsx
+++ src/app/Views/HomeView.jsx
@@ -3,21 +3,44 @@ import React, { useEffect, useState } from "react";
 import CustomButton from "@/app/Components/common/CustomButton";
 import { ClipboardDocumentListIcon } from "@heroicons/react/24/outline";
 import ListItem from "../Components/common/ListItem";
+import { useAppDispatch, useAppSelector } from "@/store/hooks";
+import {
+  addTask,
+  getTasksFromLocalStorage,
+} from "@/store/features/counter/projectSlice";
+import EmptyTodos from "../Components/alert/EmptyTodos";
 
 const HomeView = () => {
-  const [todos, setTodos] = useState([]);
+  const dispatch = useAppDispatch();
+  const [inputValue, setInputValue] = useState("");
+  const [completedTasksCount, setcompletedTasksCount] = useState(0);
+  const tasks = useAppSelector((state) => state.project.tasks);
 
   useEffect(() => {
-    const stringTodos = localStorage.getItem("todos");
-    const todos = JSON.parse(stringTodos);
-
-    if (todos && todos.length > 0) {
-      setTodos(todos);
+    if (localStorage.getItem("tasks")) {
+      const localStorageTasks = JSON.parse(localStorage.getItem("tasks"));
+      dispatch(getTasksFromLocalStorage(localStorageTasks));
     }
-  });
+  }, []);
 
+  // Calculate completed tasks
+  useEffect(() => {
+    let completedTasks = tasks.filter((task) => task.completed === true);
+    setcompletedTasksCount(completedTasks.length);
+  }, [tasks]);
+
+  // Form Submit
   const handleFormSubmit = (e) => {
     e.preventDefault();
+
+    if (inputValue) {
+      dispatch(
+        addTask({ id: tasks.length + 1, text: inputValue, completed: false })
+      );
+    }
+
+    // Refresh Input value
+    setInputValue("");
   };
 
   return (
@@ -28,12 +51,14 @@ const HomeView = () => {
       >
         {/* Form */}
         <input
-          className="w-full rounded-md p-3 outline-none border border-theme-gray-700 bg-theme-gray-500 text-theme-gray-300"
+          onChange={(e) => setInputValue(e.target.value)}
+          value={inputValue}
+          className="w-full rounded-md p-3 outline-none border border-theme-gray-700 bg-theme-gray-500 text-theme-gray-200"
           type="text"
           placeholder="Add a new task"
         />
 
-        <CustomButton buttonType={"addButton"} roundSize={2}>
+        <CustomButton type="submit" buttonType={"addButton"} roundSize={2}>
           Add
         </CustomButton>
       </form>
@@ -45,7 +70,7 @@ const HomeView = () => {
           </h1>
 
           <p className="bg-theme-gray-500 w-8 h-8 text-white rounded-full p-2 font-bold flex items-center justify-center">
-            {todos.length}
+            {tasks && tasks.length}
           </p>
         </div>
 
@@ -55,29 +80,25 @@ const HomeView = () => {
           </h1>
 
           <p className="bg-theme-gray-500 w-8 h-8 text-white rounded-full p-2 font-bold flex items-center justify-center">
-            {todos.length}
+            {completedTasksCount}
           </p>
         </div>
       </div>
 
-      {!todos.length && (
-        <div className="w-full text-center mt-16">
-          <ClipboardDocumentListIcon className="w-20  mx-auto text-theme-gray-400 mb-4" />
-
-          <h1 className="text-xl font-bold text-theme-gray-300">
-            You don't have tasks registered yet
-          </h1>
-
-          <h1 className="text-xl font-medium text-theme-gray-300">
-            Create tasks and organize your to-do items
-          </h1>
-        </div>
+      {/* Tasks */}
+      {tasks && tasks.length === 0 ? (
+        <EmptyTodos />
+      ) : (
+        tasks &&
+        tasks
+          .slice()
+          .reverse()
+          .map((index, task) => (
+            <div key={task} className="mt-3">
+              <ListItem task={index} />
+            </div>
+          ))
       )}
-
-      {todos &&
-        todos.map((task) => {
-          <ListItem task={task} />;
-        })}
     </div>
   );
 };
