5caf1823fabb49a42264738a0002b92c594f338f
umitahmedd <135972028+umitahmedd@users.noreply.github.com>
2023-12-30 16:59:18 +0300

    Update Hero.jsx

--- src/app/Components/Hero.jsx
+++ src/app/Components/Hero.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Hero = () => {
   return (
     <div className="w-full flex items-center justify-center h-[20vh] bg-theme-gray-700 ">
-      <Image src="/images/Logo.png" alt="Logo" width={150} height={100} />
+      <Image src="/images/Logo.png" alt="Logo" width={120} height={100} />
     </div>
   );
 };
